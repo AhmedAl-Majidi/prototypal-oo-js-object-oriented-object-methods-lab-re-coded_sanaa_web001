7de8c19aee8de0b5843f49558efb7cc6520a9a2a
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-14 16:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function BoardMember(name,homeState,training){
   this.homeState = homeState;
   this.training =training;
 }
+BoardMember.prototype.veto = function() {
+  return 'No, I must disagree';
+}
+BoardMember.prototype.approve = function() {
+  return 'You can do that!';
+}
+
